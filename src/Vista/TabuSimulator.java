7374fe2bbb26a28f85d9b552803bb2e0b897a780
moises <moises.quispe@holinsys.pe>
2018-12-02 19:14:59 -0500

    e

--- src/Vista/TabuSimulator.java
+++ src/Vista/TabuSimulator.java
@@ -176,6 +176,7 @@ public class TabuSimulator extends Thread{
               //  }
                 
                 mutex.release();
+              
                 //MUTEX OFF
             }
         }
