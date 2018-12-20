ce4f82351eab59bac0f1b7a4e5eab3188af5cda6
juan <fiestas.jm@pucp.pe>
2018-12-20 18:18:11 -0500

    fix secretario

--- src/Vista/frmMenuSecretary.java
+++ src/Vista/frmMenuSecretary.java
@@ -14,6 +14,7 @@ public class frmMenuSecretary extends javax.swing.JFrame {
    private usuarioBL usuarioBL ;
    private int idLog;
     public frmMenuSecretary(usuario usuarioLog) {
+        setUsuarioLog(usuarioLog);
          usuarioBL = new usuarioBL();
         initComponents();
         this.idLog=usuarioLog.getId();
